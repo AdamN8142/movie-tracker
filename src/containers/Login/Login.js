a9f8d44a7417870ec2af383c90467eab5a8deaa8
Adam Niedzwiecki <adamn8142@gmail.com>
2019-05-08 16:41:26 -0600

    Add login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react';
 
 
 class Login extends Component {
+  constructor(){
+    super()
+    this.state = {
+      email:'',
+      password:''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
   render() {
     return (
-      <div>
-        <button>ENTER</button>
-      </div>
+      <form>
+        <input onChange={this.handleChange} name="email"></input>
+        <input onChange={this.handleChange} name="password"></input>
+        <button>SUBMIT</button>
+      </form>
     )
   }
 }
