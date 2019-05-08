e2a57baffa6ee07f69941b372af36f2fa9f7e416
Adam Niedzwiecki <adamn8142@gmail.com>
2019-05-08 17:30:50 -0600

    Style nav

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import './Header.css'
+
 
 
 
@@ -8,8 +10,8 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <h1>MovieTracker</h1>
-        <nav>
+        <h1 className= "app-title">MovieTracker</h1>
+        <nav className= 'nav-bar'>
           <NavLink to='/' className='nav'>MOVIES</NavLink>
           <NavLink to='/tv' className='nav'>TV SHOW</NavLink>
           <NavLink to='/signin' className='nav'>FAVORITES</NavLink>
