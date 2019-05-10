3276aae4ebc83fe59c2b9b6a1035f4baf221f214
B3an5 <beantheo@gmail.com>
2019-05-10 17:53:36 -0600

    add favorites ids to user state

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,21 @@
 export const userReducer = (state={}, action) => {
   switch(action.type) {
     case 'SAVE_LOGIN': 
-      return { id: action.user.data.id, name: action.user.data.name}
+      return { id: action.user.data.id, name: action.user.data.name }
+    case 'SIGN_OUT':
+      return {}
+    case 'ADD_FAVORITE':
+      if(state.favorites && !state.favorites.includes(action.favorite)) {
+        return {...state, favorites: [...state.favorites, action.favorite]}
+      } else if(!state.favorites) {
+        return {...state, favorites: [action.favorite]}
+      } else {
+        return state
+      }
+    // case 'REMOVE_FAVORITE':
+    //   return {...state, favorites: state.favorites.filter((favorite) => {
+    //     return !action
+    //   })}
     default:
        return state
   }
